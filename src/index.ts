2cc8396bab4fb757a9dafb2593a725834815ac04
KostyaTretyak <ktretiak.in.ua@gmail.com>
2024-07-21 12:52:39 +0300

    Added hasBody() to export.

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ export { getRawParser } from './parsers/raw.js';
 export { getTextParser } from './parsers/text.js';
 export { getUrlencodedParser } from './parsers/urlencoded.js';
 export { BaseOptions, JsonOptions, RawOptions, TextOptions, UrlencodedOptions, BodyParser } from './types.js';
-export { typeIs } from './type-is.js';
+export { typeIs, hasBody } from './type-is.js';
 export { BodyParsers } from './body-parsers.js';
